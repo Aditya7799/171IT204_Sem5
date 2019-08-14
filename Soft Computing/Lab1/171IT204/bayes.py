4dcb1ea3530e9f74eb636f7eca4500f2c66a982d
Pragathi19 <pragathiachar98@gmail.com>
2019-08-14 16:02:48 +0530

    SC LAB1 Updated

---
+++ Soft Computing/Lab1/171IT204/bayes.py
@@ -0,0 +1,126 @@
+import csv
+import math
+
+def train(Rows,attributes,test_set):
+	numberyes=0
+	numberno=0
+	for i in range(len(Rows)):
+		if Rows[i][0]=="Yes":
+			numberyes+=1
+		if Rows[i][0]=="No":
+			numberno+=1
+
+	proYes=float(numberyes)/(numberyes+numberno)
+	proNo=float(numberno)/(numberyes+numberno)
+
+
+
+	p1yes=[0]*(len(attributes)-1)
+	p1no=[0]*(len(attributes)-1)
+	p0no=[0]*(len(attributes)-1)
+	p0yes=[0]*(len(attributes)-1)
+	for j in range(len(Rows)):
+		for i in range(1,len(attributes)):
+			if Rows[j][i]=='1' and Rows[j][0]=="Yes":
+				p1yes[i-1]+=1
+			if Rows[j][i]=='1' and Rows[j][0]=="No":
+				p1no[i-1]+=1
+			if Rows[j][i]=='0' and Rows[j][0]=="Yes":
+				p0yes[i-1]+=1
+			if Rows[j][i]=='0' and Rows[j][0]=="No":
+				p0no[i-1]+=1
+		
+	for i in range(len(p1yes)):
+		p1no[i]=p1no[i]/float(numberno)
+		p1yes[i]=p1yes[i]/float(numberyes)
+		p0no[i]=p0no[i]/float(numberno)
+		p0yes[i]=p0yes[i]/float(numberyes)
+
+
+	#finalYes=proYes
+	#finalNo=proNo
+	
+	acc=0
+	#print(test_set)
+	for j in range(len(test_set)):
+		yes_p=proYes
+		no_p=proNo
+		for i in range(1,len(attributes)):
+			if test_set[j][i]=='0':
+				yes_p*=p0yes[i-1]
+				no_p*=p0no[i-1]
+			elif test_set[j][i]=='1':
+				yes_p*=p1yes[i-1]
+				no_p*=p1no[i-1]
+				#print(yes_p)
+				#print(no_p)
+
+		if yes_p>no_p:
+			max_prob='Yes'
+		else:
+			max_prob='No'
+		if test_set[j][0]==max_prob:
+			acc+=1
+
+	result=float(acc)/len(test_set)
+	result*=100
+	return result
+
+
+def fold(dataset,i,k):
+	l=len(dataset)
+	start_index_test=l*(i-1)//k
+	end_index_test=l*i//k
+	#print(end_index_test)
+	if start_index_test==0:
+		start_index_train=end_index_test
+		end_index_train=l
+		return [dataset[start_index_train:end_index_train],dataset[start_index_test:end_index_test]]
+	elif end_index_test==l:
+		start_index_train=0
+		end_index_train=start_index_test
+		return [dataset[start_index_train:end_index_train],dataset[start_index_test:end_index_test]]
+	else:
+		start_index_train_first=0
+		end_index_train_first=start_index_test
+		start_index_train_second=end_index_test
+		end_index_train_second=l
+		#print(start_index_train_second)
+		#print(end_index_train_second)
+		new_dataset=[]
+		for i in range(start_index_test):
+			new_dataset.append(dataset[i])
+		for j in range(end_index_test,l):
+			new_dataset.append(dataset[j])
+
+		return [new_dataset,dataset[start_index_test:end_index_test]]
+
+		
+
+def main():
+	filename="SPECT.csv"
+	attributes=[]
+	rows=[]
+	with open(filename,'r') as csvfile:
+		csvreader=csv.reader(csvfile)
+
+		attributes=csvreader.next()
+		for row in csvreader:
+			rows.append(row)
+
+	#print(len(rows),len(attributes))
+	k=10
+	sum=0;
+	print(attributes)
+	for i in range(1,k+1):
+		accuracy=[]
+		after_fold=fold(rows,i,k)
+		train_set=after_fold[0]
+		test_set=after_fold[1]
+		#print(len(train_set),"-----")
+		acc=train(train_set,attributes,test_set)
+		print("Fold",i,":",acc)
+		sum=sum+acc
+	print("Average:",sum/k)
+
+main()
--- Soft Computing/Lab1/nb_naive.py
+++ Soft Computing/Lab1/nb_naive.py
@@ -6,36 +6,61 @@ data=data[1:]
 #APPLYING NAIVE BAYES ALGORITHM
 dataset_size=len(data)
 no_of_attr=len(data[0])
-#Second is YES First is NO
-prob=[[0,0] for _ in range(no_of_attr)]
+
+#1 yes 1
+#0 no 
+prob=[[0,0,0,0] for _ in range(no_of_attr)]
 yes,no=0,0
 for i in data:
 	if(i[0]=="Yes"):
 		yes+=1
 	else:
 		no+=1
+count_one=[0]*(no_of_attr)
+count_zero=[0]*(no_of_attr)
 for row in data:
 	for i in range(1,len(row)):
 		if(row[0]=="Yes" and row[i]=="1"):
 			prob[i][1]+=1
 		elif(row[0]=="No" and row[i]=="1"):
 			prob[i][0]+=1
+for row in data:
+	for i in range(1,len(row)):
+		if(row[i]=="1"):
+			count_one[i]+=1
+		elif(row[i]=="0"):
+			count_zero[i]+=1
+print(count_one)
+print(count_zero)
+
+for i in range(1,len(prob)):
+	for j in range(2):
+		if(j==0):
+			prob[i][j]/=count_one[i]
+		elif(j==1):
+			prob[i][j]/=count_one[i]
+
+
+		
 print(prob)
 #TAKE INPUT HERE
-inp=data[2]
-total=1
+inp=data[42]
+total_yes=1
+total_no=1
+
 for i in range(1,no_of_attr):
-	if(inp[i]=="1"):
-		total=total*(prob[i][1]*1.0/yes)
-	# else:
-	# # 	total=total*(prob[i][0]*1.0/no)
-	# print(total)
-den=1
-for i in range(1,no_of_attr):
-	if(inp[i]=="1"):
-		den=den*((prob[i][0]+prob[i][1])/(yes+no))
-	print(den)
-print(total/den)
+	if(inp[i]=='1'):
+		total_yes=total_yes*float(prob[i][1])
+	elif(inp[i]=='0'):
+		total_no=total_no*float(prob[i][0])
+
+print(total_yes,total_no)
+print(dataset_size)
+print(yes,no)
+a=total_yes*(yes/dataset_size)
+b=total_no*(no/dataset_size)
+print(a,b)
+print(a/(a+b))
 
 
 
