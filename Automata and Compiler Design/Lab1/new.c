deeb6ebf60f019d54cab6170dff10a8322a3bd87
Pragathi19 <pragathiachar98@gmail.com>
2019-08-22 17:03:58 +0530

    ACD Lab 22.08.19

---
+++ Automata and Compiler Design/Lab1/new.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+//Commment 1
+//Comment 2
+int main()
+{	int a,b,c;
+	writef();
+	writef();
+	writef();
+	writef();
+	writef();
+	writef();
+	writef();
+	readf("%d",&a):
+	readf("%d",&a):
+	readf("%d",&a):
+	return 0;
+//Comment 3
+/*asdasdasd
+sadsa
+sdadas
+dasdas
+*/
+}
---
+++ Automata and Compiler Design/Lab1/open.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+//Commment 1
+//Comment 2
+int main()
+{	int a,b,c;
+	printf();
+	printf();
+	printf();
+	printf();
+	printf();
+	printf();
+	printf();
+	scanf("%d",&a):
+	scanf("%d",&a):
+	scanf("%d",&a):
+	return 0;
+//Comment 3
+/*asdasdasd
+sadsa
+sdadas
+dasdas
+*/
+}
